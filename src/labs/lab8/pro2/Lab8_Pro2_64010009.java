9ac4e620c5e3ba88055e2d97389e4c62fc707db2
kornyellow <kornkubzaza@gmail.com>
2022-04-22 16:45:42 +0700

    Done

--- src/labs/lab8/pro2/Lab8_Pro2_64010009.java
+++ src/labs/lab8/pro2/Lab8_Pro2_64010009.java
@@ -1,6 +1,7 @@
 package labs.lab8.pro2;
 
-import classes.Triangle;
+import classes.Circle;
+import classes.Rectangle;
 
 import java.util.Scanner;
 
@@ -10,45 +11,67 @@ public class Lab8_Pro2_64010009 {
 
 		Scanner scanner = new Scanner(System.in);
 
-		double side1, side2, side3;
-		String color;
-		String is_filled;
-
+		double circleRadius1, circleRadius2;
 		while (true) {
-			System.out.print("Input 3 sides of the triangle: ");
-			side1 = scanner.nextDouble();
-			side2 = scanner.nextDouble();
-			side3 = scanner.nextDouble();
-			if (side1 > 0 && side2 > 0 && side3 > 0) break;
-			System.out.println("ERROR: invalid inputs, try again!");
-		}
+			System.out.print("Creating circle 1, input radius: ");
+			circleRadius1 = scanner.nextDouble();
 
-		System.out.print("The color is: ");
-		color = scanner.next();
-
-		while (true) {
-			System.out.print("The triangle is filled (y/n): ");
-			is_filled = scanner.next();
-			if (is_filled.equals("y") || is_filled.equals("n")) break;
+			if (circleRadius1 > 0) break;
 			System.out.println("ERROR: invalid input, try again!");
 		}
+		while (true) {
+			System.out.print("Creating circle 2, input radius: ");
+			circleRadius2 = scanner.nextDouble();
+
+			if (circleRadius2 > 0) break;
+			System.out.println("ERROR: invalid input, try again!");
+		}
+		System.out.println("------------------------");
+		Circle circle1 = new Circle(circleRadius1);
+		Circle circle2 = new Circle(circleRadius2);
+
+		if (circle1.compareTo(circle2) == 1) {
+			System.out.printf("The max circle's radius is %.1f\n", circle1.getRadius());
+		} else {
+			System.out.printf("The max circle's radius is %.1f\n", circle2.getRadius());
+		}
+		System.out.println("========================");
+
+		double rectangleWidth1, rectangleHeight1;
+		double rectangleWidth2, rectangleHeight2;
+		while (true) {
+			System.out.print("Creating rectangle 1, input width and height: ");
+			rectangleWidth1 = scanner.nextDouble();
+			rectangleHeight1 = scanner.nextDouble();
+
+			if (rectangleWidth1 > 0 && rectangleHeight1 > 0) break;
+			System.out.println("ERROR: invalid input, try again!");
+		}
+		while (true) {
+			System.out.print("Creating rectangle 2, input width and height: ");
+			rectangleWidth2 = scanner.nextDouble();
+			rectangleHeight2 = scanner.nextDouble();
+
+			if (rectangleWidth2 > 0 && rectangleHeight2 > 0) break;
+			System.out.println("ERROR: invalid input, try again!");
+		}
+		System.out.println("------------------------");
+		Rectangle rectangle1 = new Rectangle(rectangleWidth1, rectangleHeight1);
+		Rectangle rectangle2 = new Rectangle(rectangleWidth2, rectangleHeight2);
+
+		if (rectangle1.compareTo(rectangle2) == 1) {
+			System.out.printf("The max rectangle's width and height are %.1f, %.1f\n",
+				rectangle1.getWidth(),
+				rectangle1.getHeight()
+			);
+		} else {
+			System.out.printf("The max rectangle's width and height are %.1f, %.1f\n",
+				rectangle2.getWidth(),
+				rectangle2.getHeight()
+			);
+		}
+		System.out.println("========================");
 
 		scanner.close();
-
-		try {
-			Triangle triangle = new Triangle(side1, side2, side3);
-			triangle.setColor(color);
-			triangle.setIsFilled((is_filled.equals("y")));
-
-			System.out.println("----------------------------");
-			System.out.println(triangle);
-			System.out.println("The area is " + triangle.getArea());
-			System.out.println("The perimeter is " + triangle.getPerimeter());
-			System.out.println("The color is " + triangle.getColor());
-			System.out.println("Triangle is filled: " + triangle.isFilled());
-			
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
 	}
 }
